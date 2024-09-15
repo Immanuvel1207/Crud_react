3e49925779ff9498b39a39f42594c0e41ecd8824
Immanuvel <r.immanuvel12@gmail.com>
2024-09-15 14:51:48 +0530

    Basic Interface created

---
+++ crud/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import { useEffect, useState } from 'react';
+function App() {
+  
+  
+  return (
+    <div className="App">
+      <table className='bp4-html-table modifier'>
+        <thead>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Website</th>
+          <th>Action</th>
+        </thead>
+        <tbody>
+          
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
